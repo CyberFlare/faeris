d85d24e1fad1d33bad9b565473cee4cb83dfa8a6
CyberFlare <cyberfan1212@gmail.com>
2026-04-07 22:52:59 -0700

    can recieve hoyolab json from fairyVAC

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/dist/client/link";
 import { Button } from "@/components/ui/button"
+import { IoDownloadOutline } from "react-icons/io5";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
       <h1>Welcome to Faeris</h1>
       <p>Faeris is a ZZZ Disk Drive Scoring Tool to help you optimize your builds. To obtain your agent data, please download our extension so that we can score ALL of your agents.</p>
       <Link href="/scorer">Go to Scorer (testing)</Link>
-      <Button>Download</Button>
+      <Button><IoDownloadOutline />Download</Button>
     </main>
   )
 }
--- app/scorer/page.tsx
+++ app/scorer/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+"use client";
 
-const scorer = () => {
+import { useEffect, useState } from "react";
+
+const Scorer = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    window.addEventListener("message", (event) => {
+      if (event.data.type === "faerisVAC_SEND_DATA") {
+        console.log("Got the data!", event.data.data);
+        setData(event.data.data);
+      }
+    });
+  }, []);
+
   return (
-    <div>scorer</div>
-  )
-}
+    <div>
+      {(() => {
+        if (data) {
+          return <pre>{JSON.stringify(data, null, 2)}</pre>;
+        } else {
+          return <p>Waiting for data...</p>;
+        }
+      })()}
+    </div>
+  );
+};
 
-export default scorer
+export default Scorer;
